8d290804311cd86f730a498bbbdce59f47fe305a
Benjamin Saunders <freezecubed@gmail.com>
2025-03-06 02:01:59 -0800

    may revert

--- pyoptflight/__init__.py
+++ pyoptflight/__init__.py
@@ -4,4 +4,6 @@ from .solver import *
 from .setup import *
 from .boundary_objects import *
 # from . import initialize
-from . import plotting
+# from . import plotting
+from .initialize import *
+from .plotting import *
