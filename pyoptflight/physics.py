85f120adb21c333022be347b3d04860a7f09d8b9
Benjamin Saunders <freezecubed@gmail.com>
2025-05-28 01:33:14 -0700

    Reworked these functions again. Its not as satisfying as a lot of the other code but I've made it a bit more efficient by reducing repeated calculations in the ode.

--- pyoptflight/physics.py
+++ pyoptflight/physics.py
@@ -35,114 +35,134 @@ class StagePhysics(AutoRepr):
             for k in range(context.nstages)
         ]
 
-    def h(self, px, py, pz):
+    ### PLANETARY PROPERTIES ###
+    def h(self, pos): # px, py, pz
         """Altitude"""
-        return ca.sqrt(px**2 + py**2 + pz**2) - self.body.r_0
+        return ca.norm_2(pos) - self.body.r_0
     
-    def F_max(self, px, py, pz):
+    def g(self, pos): # px, py, pz
+        """Gravity vector"""
+        g_0 = self.body.g_0
+        r_0 = self.body.r_0
+        return -g_0*r_0**2*ca.sumsqr(pos)**(-3/2)*pos
+    
+    def wind(self, pos): # px, py
+        """Wind vector in inertial frame"""
+        omega_0 = self.body.omega_0
+        px, py, pz = ca.vertsplit(pos)
+        return ca.vertcat(-omega_0*py, omega_0*px, 0)
+    
+    def T(self, h): # h(px, py, pz)
+        """Returns the temperature at the position"""
+        return self.body.atm.T(h)
+
+    def rho(self, h): # h(px, py, pz)
+        """Local atmospheric density"""
+        return self.body.atm.rho_0*ca.exp(-h/self.body.atm.H)
+    
+    ### VELOCITY ###
+    def v_rel(self, pos, vel): # vx, vy, vz, wind(px, py)
+        """Velocity vector relative to atmosphere"""
+        wind = self.wind(pos)
+        return vel-wind
+
+    ### PROPULSION PERFORMANCE ###
+    def F_max(self, h): # h(px, py, pz)
         """Max thrust"""
-        h = self.h(px, py, pz)
         F_vac = self.stage.prop.F_vac
         F_SL = self.stage.prop.F_SL
         H = self.body.atm.H
         F_max = F_vac + (F_SL - F_vac)*ca.exp(-h/H)
         return F_max
     
-    def f_eff(self, f):
+    def f_eff(self, f): # f
         """Effective throttle output"""
         return f - f*ca.fmax(0, ca.fmin(1, (self.f_min - f)/self.delta))
     
-    def F_eff(self, px, py, pz, f):
+    def F_eff(self, h, f): # f_eff(f), F_max(h(px, py, pz))
         """Effective thrust"""
         f_eff = self.f_eff(f)
-        F_max = self.F_max(px, py, pz)
+        F_max = self.F_max(h)
         return f_eff*F_max
     
-    def Isp(self, px, py, pz):
+    def Isp(self, h): # h(px, py, pz)
         """Specific impulse"""
-        h = self.h(px, py, pz)
         Isp_vac = self.stage.prop.Isp_vac
         Isp_SL = self.stage.prop.Isp_SL
         H = self.body.atm.H
         Isp = Isp_vac + (Isp_SL - Isp_vac)*ca.exp(-h/H)
         return Isp
 
-    def g(self, px, py, pz):
-        """Gravity vector"""
-        g_0 = self.body.g_0
-        r_0 = self.body.r_0
-        return -g_0*r_0**2*(px**2 + py**2 + pz**2)**(-3/2)*ca.vertcat(px, py, pz)
-    
-    def rho(self, px, py, pz):
-        """Local atmospheric density"""
-        h = self.h(px, py, pz)
-        return self.body.atm.rho_0*ca.exp(-h/self.body.atm.H)
-    
-    def wind(self, px, py, pz):
-        """Wind vector in inertial frame"""
-        omega_0 = self.body.omega_0
-        return ca.vertcat(-omega_0*py, omega_0*px, 0)
-    
-    def v_rel(self, px, py, pz, vx, vy, vz):
-        """Velocity vector relative to atmosphere"""
-        wind = self.wind(px, py, pz)
-        vel = ca.vertcat(vx, vy, vz)
-        return vel-wind
-    
-    def vehicle_basis(self, psi, theta):
+    ### VEHICLE ORIENTATION ###    
+    def vehicle_basis(self, psi, theta): # psi, theta
         """Returns the vehicle basis vectors."""
         ebx = ca.vertcat(ca.cos(psi)*ca.cos(theta), ca.sin(psi)*ca.cos(theta), -ca.sin(theta))
         eby = ca.vertcat(-ca.sin(psi), ca.cos(psi), 0)
         ebz = ca.vertcat(ca.cos(psi)*ca.sin(theta), ca.sin(psi)*ca.sin(theta), ca.cos(theta))
         return ca.horzcat(ebx, eby, ebz)
-
-    def cos_angles(self, px, py, pz, vx, vy, vz, psi, theta):
+ 
+    def cos_angles(self, v_rel, basis): # vehicle_basis(psi, theta), v_rel(vx, vy, vz, wind(px, py))
         """Cosine angle between air-relative velocity vector and vehicle basis"""
-        basis = self.vehicle_basis(psi, theta)
-        v_rel = self.v_rel(px, py, pz, vx, vy, vz)
         v_rel = -v_rel if self.config.landing else v_rel
         return (basis.T @ v_rel)/ca.norm_2(v_rel)
-    
-    def T(self, px, py, pz):
-        """Returns the temperature at the position"""
-        h = self.h(px, py, pz)
-        return 273.15 # TODO!!
 
-    def mach_sqr(self, px, py, pz, vx, vy, vz):
+    def angles(self, v_rel, basis):
+        """Returns the angle (in radians) between air-relative velocity and the vehicle basis"""
+        return ca.acos(self.cos_angles(basis, v_rel))
+
+    ### MACH ###
+    def mach_sqr(self, h, v_rel): # v_rel(vx, vy, vz, wind(px, py)), T(h(px, py, pz))
         """Returns the square of the mach number"""
         # M = |v|/sqrt(gamma*R*T)
-        v_rel = self.v_rel(px, py, pz, vx, vy, vz)
-        T = self.T(px, py, pz)
         gamma = self.body.atm.gamma
         Rg = self.body.atm.Rg
+        T = self.T(h)
         return ca.sumsqr(v_rel)/(gamma*Rg*T)
     
-    def mach(self, px, py, pz, vx, vy, vz):
+    def mach(self, h, v_rel):
         """Returns the mach number"""
-        return ca.sqrt(self.mach_sqr(px, py, pz, vx, vy, vz))
+        return ca.sqrt(self.mach_sqr(v_rel, h))
 
-    def aero_coeffs(self, px, py, pz, vx, vy, vz, psi, theta):
-        cos_angles = self.cos_angles(px, py, pz, vx, vy, vz, psi, theta)
-        mach_sqr = self.mach_sqr(px, py, pz, vx, vy, vz)
+    ### AERO COEFFICIENTS ###
+    def aero_coeffs(self, h, v_rel, basis):
+        """Returns C_A, C_Ny, C_Nz aerodynamic coefficients"""
+        cos_angles = self.cos_angles(basis, v_rel)
+        mach_sqr = self.mach_sqr(v_rel, h)
+        C_A = self.stage.aero.C_A(cos_angles[0], mach_sqr)
+        C_Ny = self.stage.aero.C_Ny(cos_angles[1], mach_sqr)
+        C_Nz = self.stage.aero.C_Nz(cos_angles[2], mach_sqr)
+        return ca.vertcat(C_A, C_Ny, C_Nz)
 
+    ### DYNAMIC PRESSURE ###
+    def q(self, v_rel, rho):
+        """Dynamic pressure q"""
+        return 0.5*rho*ca.sumsqr(v_rel)
+    
+    ### ODE ###
     def ode(self, x, u):
         """ODE state vector x and control vector u"""
-        m, px, py, pz, vx, vy, vz = x[0], x[1], x[2], x[3], x[4], x[5], x[6]
+        # deconstruct state vectors
+        m = x[0]
+        pos = x[1:4]
+        vel = x[4:7]
         f, psi, theta = u[0], u[1], u[2]
 
-        coeffs = self.aero_coeffs()
+        # Get intermediaries
+        A_ref = self.stage.aero.A_ref
+        h = self.h(pos)
+        F_eff = self.F_eff(h, f)
+        Isp = self.Isp(h)
+        g = self.g(pos)
+        rho = self.rho(h)
+        v_rel = self.v_rel(pos, vel)
+        basis = self.vehicle_basis(psi, theta)
+        
+        # Get aerodynamic coefficients
+        coeffs = self.aero_coeffs(h, v_rel, basis)
         C_A = coeffs[0]
         C_Ny = coeffs[1]
         C_Nz = coeffs[2]
-
-        A_ref = self.stage.aero.A_ref
-        F_eff = self.F_eff(px, py, pz, f)
-        Isp = self.Isp(px, py, pz)
-        g = self.g(px, py, pz)
-        rho = self.rho(px, py, pz)
-        v_rel = self.v_rel(px, py, pz, vx, vy, vz)
-
-        basis = self.vehicle_basis(psi, theta)
+        
         ebx = basis[0:3]
         eby = basis[3:6]
         ebz = basis[6:9]
@@ -158,14 +178,4 @@ class StagePhysics(AutoRepr):
         # vy_dot = g[1] + F_eff/m*ebx[1] + 0.5/m*rho*A_ref*ca.norm_2(v_rel)*C_A*v_rel[1]
         # vz_dot = g[2] + F_eff/m*ebx[2] + 0.5/m*rho*A_ref*ca.norm_2(v_rel)*C_A*v_rel[2]
 
-        return ca.vertcat(m_dot, vx, vy, vz, v_dot)
-    
-    def alpha(self, px, py, pz, vx, vy, vz, psi, theta):
-        """AoA in rads"""
-        return ca.acos(self.cos_angles(px, py, pz, vx, vy, vz, psi, theta)[0])
-
-    def q(self, px, py, pz, vx, vy, vz):
-        """Dynamic pressure q"""
-        rho = self.rho(px, py, pz)
-        v_rel = self.v_rel(px, py, pz, vx, vy, vz)
-        return 0.5*rho*ca.sumsqr(v_rel)
+        return ca.vertcat(m_dot, vel, v_dot)
